d88f506354330ab4a239eb25c188480c9f02600f
bengroves93 <ben.groves1993@gmail.com>
2016-11-08 11:18:55 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# aws_accesskeys
-tool to list AWS IAM user access keys from a given account
+# AWS Accesskeys
+Tool to list AWS IAM user access keys from a given account
+
+### Required:
+
+- ruby version 2.2.4
+
+Gems - 
+```sh
+$ gem install aws-sdk
+$ gem install trollop
+```
+
+
+### Usage: 
+
+```sh
+$ ruby user_keys.rb -h
+Options:
+  -i, --id=<s>     Your AWS access key ID
+  -k, --key=<s>    Your AWS Secret Access Key
+  -h, --help       Show this message
+```
+
+```sh
+$ ruby user_keys.rb -i AKIAPSOPG2DSIEXAMPLE -k EX4mP1e+4Ws-5eCre7+4cc3ss=k3Y
+```
---
+++ user_keys.rb
@@ -0,0 +1,28 @@
+require 'aws-sdk'
+require 'json'
+require 'trollop'
+
+opts = Trollop::options do
+	opt :id , "Your AWS access key ID", :required => true, type: :string
+	opt :key, "Your AWS Secret Access Key", :required => true, type: :string
+end
+
+iam = Aws::IAM::Client.new(
+  region: 'us-west-2',
+  access_key_id: opts[:id],
+  secret_access_key: opts[:key]
+)
+
+keyList = iam.list_access_keys
+keys = {}
+keyList.access_key_metadata.each do |accesskey|
+  
+  if keys[accesskey.user_name]
+     keys[accesskey.user_name].push accesskey.access_key_id
+  else
+     keys[accesskey.user_name] = [accesskey.access_key_id]
+  end
+  
+  
+end
+puts keys.to_json
